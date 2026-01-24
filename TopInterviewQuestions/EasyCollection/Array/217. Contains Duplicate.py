30874242a369e94578cf45d6cf5a3c82aa1a0b47
Sith_PJ <sithi.advt@gmail.com>
2026-01-25 02:01:15 +0700

    good

--- TopInterviewQuestions/EasyCollection/Array/217. Contains Duplicate.py
+++ TopInterviewQuestions/EasyCollection/Array/217. Contains Duplicate.py
@@ -1,3 +1,10 @@
+"""
+LeetCode Problem: 17. Contains Duplicate
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Link: https://leetcode.com/problems/contains-duplicate/description/
+"""
+
 class Solution(object):
     def containsDuplicate(self, nums):
         duplicates = set()
--- TopInterviewQuestions/EasyCollection/Strings/242. Valid Anagram.py
+++ TopInterviewQuestions/EasyCollection/Strings/242. Valid Anagram.py
@@ -1,3 +1,10 @@
+"""
+LeetCode Problem: 242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise
+
+Link: https://leetcode.com/problems/valid-anagram/description/
+"""
+
 class Solution(object):
     def isAnagram(self, s, t):
         if len(s) != len(t):
